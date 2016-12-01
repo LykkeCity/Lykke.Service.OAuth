2c0ea7cbb80b913d224ed04290f777091c848b0f
Dmitry Samodov <samodov.dmitry@gmail.com>
2016-12-01 16:31:59 +0300

    moved email templates

--- src/BusinessService/BusinessServiceConfig.cs
+++ src/BusinessService/BusinessServiceConfig.cs
@@ -1,9 +1,12 @@
 ﻿using BusinessService.Clients;
 using BusinessService.Country;
 using BusinessService.Infrastructure;
+using BusinessService.Messages.Email;
+using BusinessService.Messages.Settings;
 using Common;
 using Core.Clients;
 using Core.Country;
+using Core.Messages.Email;
 using Core.Settings;
 using StructureMap;
 
@@ -23,6 +26,17 @@ namespace BusinessService
                 _.AddAllTypesOf<IApplicationService>();
                 _.WithDefaultConventions();
             });
+
+            var serviceBusSettings = new ServiceBusEmailSettings
+            {
+                Key = settings.EmailServiceBus.Key,
+                QueueName = settings.EmailServiceBus.QueueName,
+                NamespaceUrl = settings.EmailServiceBus.NamespaceUrl,
+                PolicyName = settings.EmailServiceBus.PolicyName
+            };
+
+            For<ServiceBusEmailSettings>().Use(serviceBusSettings).Singleton();
+            For<IEmailSender>().Use<ServiceBusEmailSender>();
 
             For<IRegistrationConsumer>().Use<JobGeolocationDataUpdater>();
             For<ICountryService>().Use<CountryService>().Ctor<IBaseSettings>().Is(settings);
