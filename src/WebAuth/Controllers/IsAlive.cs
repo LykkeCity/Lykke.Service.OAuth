a3753b17273f612ea0359abd1dba492661fdb3f2
Andrey Anokhin <andrey.anokhin@lykke.com>
2017-05-27 00:06:23 +0300

    Added is alive

---
+++ src/WebAuth/Controllers/IsAlive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAuth.Controllers
+{
+    [Route("api/[controller]")]
+    public class IsAlive : Controller
+    {
+        [HttpGet]
+        public IsAliveResponse Get()
+        {
+            return new IsAliveResponse
+            {
+                Version =
+                    Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion,
+                Env = Environment.GetEnvironmentVariable("ENV_INFO")
+            };
+        }
+
+        public class IsAliveResponse
+        {
+            public string Version { get; set; }
+            public string Env { get; set; }
+        }
+    }
+}
