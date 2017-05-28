73ab1552e6ed227d976c8d965f3be69c77739e8a
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2017-05-29 00:08:25 +0400

    Add certificates.

--- src/WebAuth/Program.cs
+++ src/WebAuth/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using System;
+using AzureStorage.Blob;
+using System.Security.Cryptography.X509Certificates;
+using Common;
 
 namespace WebAuth
 {
@@ -7,15 +11,49 @@ namespace WebAuth
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseUrls("http://*:5000")
-                .UseStartup<Startup>()
-                .Build();
+            var sertConnString = Environment.GetEnvironmentVariable("CertConnectionString");
 
-            host.Run();
+            if (string.IsNullOrWhiteSpace(sertConnString) || sertConnString.Length < 10)
+            {
+
+                var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseUrls("http://*:5000/")
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .Build();
+
+                host.Run();
+
+            }
+            else
+            {
+                var sertContainer = Environment.GetEnvironmentVariable("CertContainer");
+                var sertFilename = Environment.GetEnvironmentVariable("CertFileName");
+                var sertPassword = Environment.GetEnvironmentVariable("CertPassword");
+
+                var certBlob = new AzureBlobStorage(sertConnString);
+                var cert = certBlob.GetAsync(sertContainer, sertFilename).Result.ToBytes();
+
+                X509Certificate2 xcert = new X509Certificate2(cert, sertPassword);
+
+                var host = new WebHostBuilder()
+                    .UseKestrel(x =>
+                    {
+                        x.UseHttps(xcert);
+                        x.AddServerHeader = false;
+
+                    })
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseUrls("https://*:443/")
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .Build();
+
+                host.Run();
+            }
         }
     }
 }
+
