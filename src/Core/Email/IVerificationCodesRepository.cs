feab1d5a07c306d96bb28118e067e983a2f96e09
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-04-06 00:09:15 +0300

    save cid and traffic on user registratio

--- src/Core/Email/IVerificationCodesRepository.cs
+++ src/Core/Email/IVerificationCodesRepository.cs
@@ -10,11 +10,13 @@ namespace Core.Email
         int ResendCount { get; }
         string Referer { get; }
         string ReturnUrl { get; }
+        string Cid { get; }
+        string Traffic { get; }
     }
 
     public interface IVerificationCodesRepository
     {
-        Task<IVerificationCode> AddCodeAsync(string email, string referer, string returnUrl);
+        Task<IVerificationCode> AddCodeAsync(string email, string referer, string returnUrl, string cid, string traffic);
         Task<IVerificationCode> GetCodeAsync(string key);
         Task<IVerificationCode> UpdateCodeAsync(string key);
         Task DeleteCodesAsync(string email);
