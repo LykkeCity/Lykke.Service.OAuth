de08daad313850e161c3d1b193cb769fd9e6b7ee
Mikhail Musin <mikhail.musin@gmail.com>
2018-04-11 01:56:02 +0300

    LWDEV-6838 application type on auth

--- src/WebAuth/Controllers/UserinfoController.cs
+++ src/WebAuth/Controllers/UserinfoController.cs
@@ -14,6 +14,7 @@ using Lykke.Service.ClientAccount.Client.Models;
 using Lykke.Service.Kyc.Abstractions.Domain.Profile;
 using Lykke.Service.Kyc.Abstractions.Services;
 using Lykke.Service.Session;
+using Lykke.Service.Session.Client;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebAuth.Extensions;
@@ -26,7 +27,7 @@ namespace WebAuth.Controllers
         private readonly ILog _log;
         private readonly IApplicationRepository _applicationRepository;
         private readonly IKycProfileServiceV2 _kycProfileService;
-        private readonly IClientsSessionsRepository _clientSessionsClient;
+        private readonly IClientSessionsClient _clientSessionsClient;
         private readonly IWalletCredentialsRepository _walletCredentialsRepository;
         private readonly IClientAccountClient _clientAccountClient;
 
@@ -34,7 +35,7 @@ namespace WebAuth.Controllers
             ILog log,
             IApplicationRepository applicationRepository,
             IKycProfileServiceV2 kycProfileService,
-            IClientsSessionsRepository clientSessionsClient,
+            IClientSessionsClient clientSessionsClient,
             IWalletCredentialsRepository walletCredentialsRepository,
             IClientAccountClient clientAccountClient)
         {
@@ -158,7 +159,7 @@ namespace WebAuth.Controllers
 
             try
             {
-                var authResult = await _clientSessionsClient.Authenticate(clientAccount.Id, "oauth server");
+                var authResult = await _clientSessionsClient.Authenticate(clientAccount.Id, "oauth server", application: app.Type);
                 return Json(new { Token = authResult.SessionToken });
             }
             catch (Exception ex)
