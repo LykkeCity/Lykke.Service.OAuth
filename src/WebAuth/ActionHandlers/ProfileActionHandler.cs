3ec3bbc977baf423b603e1f93289a41fa7e7d39c
Emmanuel <emanx140@yandex.ru>
2018-05-16 00:15:13 +0500

    Revert "Replace using ClientAccount service for Email to use PersonalData service instead."

--- src/WebAuth/ActionHandlers/ProfileActionHandler.cs
+++ src/WebAuth/ActionHandlers/ProfileActionHandler.cs
@@ -6,7 +6,6 @@ using Core.Extensions;
 using Lykke.Service.ClientAccount.Client;
 using Lykke.Service.Kyc.Abstractions.Services;
 using Lykke.Service.Kyc.Abstractions.Services.Models;
-using Lykke.Service.PersonalData.Contract;
 using Lykke.Service.PersonalData.Contract.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
@@ -21,21 +20,18 @@ namespace WebAuth.ActionHandlers
         private readonly IUserManager _userManager;
         private readonly IClientAccountClient _clientAccountClient;
         private readonly IKycProfileService _kycProfileService;
-        private readonly IPersonalDataService _personalDataService;
 
         public ProfileActionHandler(
             IHttpContextAccessor httpContextAccessor,
             IUserManager userManager,
             IClientAccountClient clientAccountClient,
-            IKycProfileService kycProfileService,
-            IPersonalDataService personalDataService
+            IKycProfileService kycProfileService
             )
         {
             _httpContextAccessor = httpContextAccessor;
             _userManager = userManager;
             _clientAccountClient = clientAccountClient;
             _kycProfileService = kycProfileService;
-            _personalDataService = personalDataService;
         }
 
         public async Task UpdatePersonalInformation(string clientId, string firstName, string lastName)
@@ -56,11 +52,10 @@ namespace WebAuth.ActionHandlers
             await _kycProfileService.UpdatePersonalDataAsync(clientId, changes);
 
             var clientAccount = await _clientAccountClient.GetByIdAsync(clientId);
-            var clientEmail = await _personalDataService.GetEmailAsync(clientId);
 
             await _httpContextAccessor.HttpContext.SignOutAsync(OpenIdConnectConstantsExt.Auth.DefaultScheme);
 
-            var identity = await _userManager.CreateUserIdentityAsync(clientAccount.Id, clientEmail, clientEmail, true);
+            var identity = await _userManager.CreateUserIdentityAsync(clientAccount.Id, clientAccount.Email, clientAccount.Email, true);
 
             await _httpContextAccessor.HttpContext.SignInAsync(OpenIdConnectConstantsExt.Auth.DefaultScheme,
                     new ClaimsPrincipal(identity),
