e4fbfc38cd278f0c1c06c78e216eff4fa775393f
Georgii Afanasev <georgii.afanasev@lykke.com>
2018-05-15 16:34:39 +0400

    Add links to lykke.com for logos.

---
+++ src/WebAuth/Models/LykkeLogoViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAuth.Models
+{
+    public class LykkeLogoViewModel
+    {
+        public string Url { get; set; }
+    }
+}
---
+++ src/WebAuth/ViewComponents/LykkeLogoViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAuth.Models;
+
+namespace WebAuth.ViewComponents
+{
+    public class LykkeLogoViewComponent : ViewComponent
+    {
+        public string DefaultUrl { get; set; }
+
+        public LykkeLogoViewComponent()
+        {
+            DefaultUrl = "https://www.lykke.com/";
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string url)
+        {
+            var href = String.IsNullOrEmpty(url) ? DefaultUrl : url;
+            var model = new LykkeLogoViewModel
+            {
+                Url = href,
+            };
+
+            return View(model);
+        }
+    }
+}
