4f5ef6aace95d24272c5ccd40dc0456dc92ee629
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2018-08-10 13:54:01 +0200

    Update nugets

--- src/Lykke.Service.OAuth/Startup.cs
+++ src/Lykke.Service.OAuth/Startup.cs
@@ -23,6 +23,7 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 using WebAuth.EventFilter;
 using WebAuth.Providers;
 using WebAuth.Modules;
@@ -103,7 +104,13 @@ namespace WebAuth
 
                 var builder = new ContainerBuilder();
                 var settings = Configuration.LoadSettings<AppSettings>();
+
+
                 _settings = settings.CurrentValue;
+
+                Configuration.CheckDependenciesAsync(settings,_settings.SlackNotifications.AzureQueue.ConnectionString,_settings.SlackNotifications.AzureQueue.QueueName,
+                    $"{PlatformServices.Default.Application.ApplicationName} {PlatformServices.Default.Application.ApplicationVersion}");
+
 
                 Log = CreateLogWithSlack(services, settings);
 
