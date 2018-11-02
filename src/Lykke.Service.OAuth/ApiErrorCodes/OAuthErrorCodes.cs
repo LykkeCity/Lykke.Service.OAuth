9baa70c0a12a40030e3a16d5db099beace1a703f
Kobzev Michael <michael.kobzev@lykke.com>
2018-11-02 16:16:00 +0300

    feature-t1-26-client-validation: added validation for client id

---
+++ src/Lykke.Service.OAuth/ApiErrorCodes/OAuthErrorCodes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Core.PasswordValidation;
+using Lykke.Common.ApiLibrary.Contract;
+
+namespace Lykke.Service.OAuth.ApiErrorCodes
+{
+    /// <summary>
+    ///     Class for OAuth-related errors.
+    /// </summary>
+    public static class OAuthErrorCodes
+    {
+        /// <summary>
+        /// Client not found
+        /// </summary>
+        public static ILykkeApiErrorCode ClientNotFound = new LykkeApiErrorCode(nameof(ClientNotFound), "Client not found.");
+    }
+}
