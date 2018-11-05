cc3980b429df9cf2a8b2135f43f9cca9ad85b8d6
Kobzev Michael <michael.kobzev@lykke.com>
2018-11-05 09:26:43 +0300

    feature-t1-33-registration-id-reuse: reuse of registration id added

--- src/BusinessService/RegistrationRedisRepository.cs
+++ src/BusinessService/RegistrationRedisRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Common;
 using Core.Exceptions;
 using Core.Registration;
 using MessagePack;
@@ -10,6 +11,7 @@ namespace BusinessService
     public class RegistrationRedisRepository : IRegistrationRepository
     {
         private const string RedisPrefix = "OAuth:Registration:";
+        private const string EmailRedisPrefix = "OAuth:Registration:Email:";
         private readonly IDatabase _database;
         private readonly TimeSpan _registrationExpiration;
 
@@ -24,11 +26,31 @@ namespace BusinessService
 
         public async Task<string> AddAsync(RegistrationModel entity)
         {
+            await ReuseRegistrationIdIfPossible(entity);
+
             var redisKey = ToRedisKey(entity.RegistrationId);
+
             var value = MessagePackSerializer.Serialize(entity);
+
             await _database.StringSetAsync(redisKey, value, _registrationExpiration);
 
             return entity.RegistrationId;
+        }
+
+        private async Task ReuseRegistrationIdIfPossible(RegistrationModel entity)
+        {
+            var emailRedisKey = EmailRedisPrefix + entity.Email.CalculateHash32();
+
+            var registrationId = await _database.StringGetAsync(emailRedisKey);
+            var isEmailAlreadyUsed = !registrationId.IsNull;
+            if (isEmailAlreadyUsed)
+            {
+                entity.SetRegistrationId(registrationId.ToString());
+            }
+            else
+            {
+                await _database.StringSetAsync(emailRedisKey, entity.RegistrationId, _registrationExpiration);
+            }
         }
 
         public async Task<RegistrationModel> GetAsync(string registrationId)
