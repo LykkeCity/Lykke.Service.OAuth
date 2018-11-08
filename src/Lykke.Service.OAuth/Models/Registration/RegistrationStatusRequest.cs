bd4c00ac990111c0f283da469587f592c71536e6
Georgii Afanasev <gmafsgu@gmail.com>
2018-11-08 19:46:02 +0400

    * Add secure registration status method. * Add error response models for countries.

---
+++ src/Lykke.Service.OAuth/Models/Registration/RegistrationStatusRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lykke.Service.OAuth.Models.Registration
+{
+    /// <summary>
+    ///     Request object for registration status.
+    /// </summary>
+    public class RegistrationStatusRequest
+    {
+        /// <summary>
+        ///     Registration id token.
+        /// </summary>
+        [Required]
+        public string RegistrationId { get; set; }
+    }
+}
