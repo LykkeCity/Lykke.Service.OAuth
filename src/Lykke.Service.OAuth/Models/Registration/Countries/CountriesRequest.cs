48859a7995fd7b686116fcdf03817514033e7dac
Georgii Afanasev <gmafsgu@gmail.com>
2018-11-08 13:08:11 +0400

    * Add country autodetection by ip.

---
+++ src/Lykke.Service.OAuth/Models/Registration/Countries/CountriesRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lykke.Service.OAuth.Models.Registration.Countries
+{
+    /// <summary>
+    ///     Request object for countries.
+    /// </summary>
+    public class CountriesRequest
+    {
+        /// <summary>
+        ///     Registration id token.
+        /// </summary>
+        [Required]
+        public string RegistrationId { get; set; }
+    }
+}
---
+++ tests/WebAuth.Tests/Countries/CountriesServiceOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Lykke.Common;
+using Lykke.Service.IpGeoLocation;
+using NSubstitute;
+
+namespace WebAuth.Tests.Countries
+{
+    /// <summary>
+    ///     Util class for configuring test countries service.
+    /// </summary>
+    internal class CountriesServiceOptions
+    {
+        public IEnumerable<CountryItem> CountryItems { get; set; }
+        public IEnumerable<string> RestrictedCountriesOfResidenceIso2 { get; set; }
+        public IIpGeoLocationClient GeoLocationClient { get; set; }
+
+        public CountriesServiceOptions()
+        {
+            CountryItems = new List<CountryItem>();
+            RestrictedCountriesOfResidenceIso2 = new List<string>();
+            GeoLocationClient = Substitute.For<IIpGeoLocationClient>();
+        }
+    }
+}
