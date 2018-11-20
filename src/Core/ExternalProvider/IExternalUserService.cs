74251cdd368efc7963eef97d9e27604832ddb0e6
Georgii Afanasev <gmafsgu@gmail.com>
2018-11-20 21:12:00 +0400

    Add ironclad as proxy for Lykke Oauth authentication.

---
+++ src/Core/ExternalProvider/IExternalUserService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Core.Exceptions;
+
+namespace Core.ExternalProvider
+{
+    /// <summary>
+    ///     External user operations.
+    /// </summary>
+    public interface IExternalUserService
+    {
+        /// <summary>
+        ///     Associate lykke user with external user.
+        /// </summary>
+        //TODO:@gafanasiev Think if we need "provider" here. Or we can use ironclad for associating any account.
+        /// <param name="provider">External provider name.</param>
+        /// <param name="externalUserId">External user id.</param>
+        /// <param name="lykkeUserId">Lykke user id.</param>
+        /// <returns>Completed Task if user was associated.</returns>
+        /// <exception cref="ExternalUserAlreadyAssociatedException">Thrown when external user already associated with lykke user.</exception>
+        Task AssociateExternalUserAsync(string provider, string externalUserId, string lykkeUserId);
+
+        /// <summary>
+        ///     Get associated lykke user id.
+        /// </summary>
+        /// <param name="provider">External provider name.</param>
+        /// <param name="externalUserId">External user id.</param>
+        /// <returns>
+        ///     Lykke user id if it was already associated.
+        ///     Empty string if user is not associated.
+        /// </returns>
+        Task<string> GetAssociatedLykkeUserIdAsync(string provider, string externalUserId);
+    }
+}
---
+++ src/Lykke.Service.OAuth.Services/ExternalProvider/ExternalProviderService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.ExternalProvider;
+using StackExchange.Redis;
+
+namespace Lykke.Service.OAuth.Services.ExternalProvider
+{
+    public class ExternalProviderService : IExternalProviderService
+    {
+        private const string RedisPrefix = "OAuth:IroncladLykkeLogins";
+        private readonly IDatabase _database;
+
+        public ExternalProviderService(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _database = connectionMultiplexer.GetDatabase();
+        }
+
+        public async Task<string> SaveLykkeUserIdForExternalLoginAsync(string lykkeUserId, TimeSpan ttl)
+        {
+            if (string.IsNullOrWhiteSpace(lykkeUserId))
+                throw new ArgumentNullException(nameof(lykkeUserId));
+
+            var guid = Guid.NewGuid().ToString();
+
+            var redisKey = GetRedisKey(guid);
+            await _database.StringSetAsync(redisKey, lykkeUserId, ttl);
+            return guid;
+        }
+
+        public async Task<string> GetLykkeUserIdForExternalLoginAsync(string guid)
+        {
+            if (string.IsNullOrWhiteSpace(guid))
+                throw new ArgumentNullException(nameof(guid));
+
+            var redisKey = GetRedisKey(guid);
+
+            var lykkeUserId = await _database.StringGetAsync(redisKey);
+
+            if (!lykkeUserId.HasValue)
+                return string.Empty;
+
+            return lykkeUserId;
+        }
+
+        private string GetRedisKey(string guid)
+        {
+            if (string.IsNullOrWhiteSpace(guid))
+                throw new ArgumentNullException(nameof(guid));
+
+            return $"{RedisPrefix}:{guid}";
+        }
+    }
+}
