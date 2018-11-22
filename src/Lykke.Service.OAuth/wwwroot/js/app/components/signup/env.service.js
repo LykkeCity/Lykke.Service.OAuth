3e1de811192f2734478830a7d66b2b4fbe892ed5
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-11-22 11:49:35 +0300

    feature-FE-111-implement-auth-token-on-website: added final registration flow

---
+++ src/Lykke.Service.OAuth/wwwroot/js/app/components/signup/env.service.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('envService', envService);
+
+    envService.$inject = ['$window'];
+
+    function envService($window) {
+
+        function getFundsUrl() {
+            var fundsUrls = {
+                'localhost:5000': 'http://localhost:3000',
+                'auth.lykke.com': 'http://wallet.lykke.com',
+                'auth-dev.lykkex.net': 'http://webwallet-dev.lykkex.net',
+                'auth-test.lykkex.net': 'http://webwallet-test.lykkex.net'
+            }
+
+            return fundsUrls[$window.location.host];
+        }
+
+        return {
+            getFundsUrl: getFundsUrl
+        };
+    }
+})();
