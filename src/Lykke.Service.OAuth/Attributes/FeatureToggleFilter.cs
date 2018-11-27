c7144d28f0221835e9759a2700936116355ad41d
Kobzev Michael <michael.kobzev@lykke.com>
2018-11-27 11:59:26 +0300

    feature/LAUTH-83: feature toggle added

---
+++ src/Lykke.Service.OAuth/Attributes/FeatureToggleFilter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Lykke.Service.OAuth.Settings.ServiceSettings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lykke.Service.OAuth.Attributes
+{
+    public class FeatureToggleFilter : ActionFilterAttribute
+    {
+        private readonly List<string> _features;
+        private readonly FeatureToggleSettings _featureToggleSettings;
+
+        public FeatureToggleFilter(FeatureToggleSettings settings, string features)
+        {
+            _featureToggleSettings = settings;
+            _features = features.Split(',').ToList();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            foreach (var feature in _features)
+            {
+                var featureToogle = _featureToggleSettings.DisabledFeatures.FirstOrDefault(x => x == feature);
+
+                if (featureToogle != null)
+                {
+                    context.Result = new BadRequestObjectResult("Feature is disabled");
+                }
+            }
+        }
+    }
+}
---
+++ src/Lykke.Service.OAuth/Settings/ServiceSettings/FeatureToggleSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Lykke.Service.OAuth.Settings.ServiceSettings
+{
+    public class FeatureToggleSettings
+    {
+        public IEnumerable<string> DisabledFeatures { get; set; }
+    }
+}
