85dfba440b394fc7819ed6e97fd8a134ae931468
Georgii Afanasev <gmafsgu@gmail.com>
2018-12-12 20:44:07 +0400

    Revert "Add ironclad as proxy for Lykke Oauth authentication."
    This reverts commit 9c8d27be7ae60aa6260085b63ec42fcb73b19295.

--- src/Core/Extensions/OpenIdConnectConstantsExt.cs
+++ src/Core/Extensions/OpenIdConnectConstantsExt.cs
@@ -10,19 +10,9 @@
             public const string SessionId = "http://lykke.com/oauth/sessionid";
         }
 
-        public static class Parameters
-        {
-            public const string PartnerIdParameter = "partnerId";
-            public const string AfterExternalLoginCallback = "after_external_login_callback";
-            public const string Tenant = "tenant";
-        }
-
         public static class Auth
         {
             public const string DefaultScheme = "ServerCookie";
-            public const string ExternalAuthenticationScheme = "ExternalAuthenticationScheme";
-            public const string IroncladAuthenticationScheme = "IroncladAuthenticationScheme";
-
         }
 
         public static class Errors
@@ -30,27 +20,6 @@
             public const string UnknownSession = "lykke_unknown_session";
             public const string ClaimNotFound = "lykke_claim_not_found";
             public const string ClientBanned = "lykke_client_banned";
-        }
-
-        public static class Tenants
-        {
-            public const string Ironclad = "ironclad";
-        }
-
-        public static class Providers
-        {
-            public const string Ironclad = "ironclad";
-        }
-
-        public static class Protectors
-        {
-            public const string ExternalProviderCookieProtector = "ExternalProviderCookieProtector";
-        }
-
-        public static class Cookies
-        {
-            public const string TemporaryUserIdCookie = "TemporaryUserIdCookie";
-
         }
     }
 }
