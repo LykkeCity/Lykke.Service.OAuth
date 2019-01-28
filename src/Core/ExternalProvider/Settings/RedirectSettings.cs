98d40703006ed9f25c364758a1d6a15c9baabeea
Georgii Afanasev <gmafsgu@gmail.com>
2019-01-29 00:04:24 +0400

    LAUTH-120 LAUTH-121 * Possible fix for ios.

--- src/Core/ExternalProvider/Settings/RedirectSettings.cs
+++ src/Core/ExternalProvider/Settings/RedirectSettings.cs
@@ -6,5 +6,10 @@
         ///     Acr values used during old lykke flow, passed to ironclad authentication.
         /// </summary>
         public string OldLykkeSignInIroncladAuthAcrValues { get; set; }
+
+        /// <summary>
+        ///     Minimum version of ios to replace 302 request with 200 html form.
+        /// </summary>
+        public int IosMinVersionForCustomRedirect { get; set; }
     }
 }
